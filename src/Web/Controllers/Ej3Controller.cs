2716492a44048e0ff406aaaa165538b4b2f5eef3
ivanapanowicz <ivanapanowicz@gmail.com>
2024-08-07 16:27:51 -0300

    Ejercicio 3 resuelto

---
+++ src/Web/Controllers/Ej3Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3Controller : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string name, int age)
+        {
+            return $"Te llamas {name} y tienes ” {age} ” años";
+        }
+    }
+}
