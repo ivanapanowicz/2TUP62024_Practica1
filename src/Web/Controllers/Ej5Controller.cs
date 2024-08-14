746529a510fbb1541b90f5786263d70e0c8c727b
ivanapanowicz <ivanapanowicz@gmail.com>
2024-08-14 14:51:44 -0300

    Ejercicio 5 resuelto

---
+++ src/Web/Controllers/Ej5Controller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string day)
+        {
+            string days = day.ToLower().Trim();
+
+            List<string> weekdays = new List<string> { "lunes", "martes", "miércoles", "miercoles", "jueves", "viernes" };
+            List<string> weekends = new List<string> { "sábado", "sabado", "domingo" };
+
+            if (weekends.Contains(days))
+            {
+                return Ok("Es fin de semana");
+            }
+            else if (weekdays.Contains(days))
+            {
+                return Ok("No es fin de semana");
+            }
+            else
+            {
+                return BadRequest("Día no válido.");
+            }
+        }
+    }
+}
