d42073c62e665d71f96c80b7f5f44e477442c620
ivanapanowicz <ivanapanowicz@gmail.com>
2024-08-14 14:57:38 -0300

    Ejercicio 9 resuelto

---
+++ src/Web/Controllers/Ej9Controller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej9Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(int inicio, int final)
+        {
+            if (final - inicio >= 1000)
+            {
+                return BadRequest("La diferencia entre inicio y fin debe ser menor a 1000.");
+            }
+
+            List<int> numbers = new List<int>();
+
+            for (int i = inicio; i <= final; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    numbers.Add(i);
+                }
+            }
+
+            return Ok(numbers);
+        }
+    }
+}
