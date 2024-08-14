b650fb2b9fc319633a1fb75fc3de49a91fa8451d
ivanapanowicz <ivanapanowicz@gmail.com>
2024-08-14 14:57:02 -0300

    Ejercicio 8 resuelto

---
+++ src/Web/Controllers/Ej8Controller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej8Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(int inicio, int final)
+        {
+            if (final - inicio >= 1000)
+            {
+                return BadRequest("La diferencia entre inicio y fin debe ser menor a 1000.");
+            }
+
+            List<int> numbers = new List<int>();
+
+            int i = inicio;
+
+            while (i <= final) 
+            {
+                numbers.Add(i); 
+                i++;
+            }
+
+            return Ok(numbers);
+        }
+    }
+}
