e65505443814ef675225f52a266237f250228cd4
ivanapanowicz <ivanapanowicz@gmail.com>
2024-08-21 12:11:38 -0300

    Ej 1 LINQ

---
+++ src/Web/Controllers/Ej1LINQController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej1LINQController : ControllerBase
+    {
+        [HttpGet]
+        public List<int> Get([FromQuery]List<int> list)
+        {
+            var filtrarLista = list.Where(x => x > 30 && 100 < x).ToList();
+            return filtrarLista;
+        }
+    }
+}
