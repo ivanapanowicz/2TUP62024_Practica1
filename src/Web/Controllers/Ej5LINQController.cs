a1e3d06445381980f32dbdf8c932e6f6193fa810
ivanapanowicz <ivanapanowicz@gmail.com>
2024-08-21 12:17:31 -0300

    Ej 5 LINQ

---
+++ src/Web/Controllers/Ej5LINQController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5LINQController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get([FromQuery] List<int> list)
+        {
+            var query = list.Select(x => (Num: x, Cuadrado: x * x)).Where(result => result.Cuadrado > 20).Select(result => $"{result.Num}-{result.Cuadrado}");
+
+            return query;
+
+        }
+    }
+}
