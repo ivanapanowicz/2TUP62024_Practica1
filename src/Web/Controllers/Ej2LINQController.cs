7d7a1c06e71c3ad172b575766083d62f4eadb641
ivanapanowicz <ivanapanowicz@gmail.com>
2024-08-21 12:12:38 -0300

    Ej 2 LINQ

---
+++ src/Web/Controllers/Ej2LINQController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej2LINQController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get([FromQuery] List<string> words)
+        {
+            var query = words.Where(w => w.Length >= 5).Select(w => w.ToUpper());
+            return query;
+        }
+    }
+}
