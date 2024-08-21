bc02788d2e047f53ebe38c631987d9e40d60aef2
ivanapanowicz <ivanapanowicz@gmail.com>
2024-08-21 12:15:29 -0300

    Ej 3 LINQ

---
+++ src/Web/Controllers/Ej3LINQController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3LINQController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get([FromQuery] List<string> word)
+        {
+            var query = word.Where(w => w.StartsWith("b") && w.EndsWith("r"));
+            return query;
+        }
+    }
+}
