1f5c393df25d8425c1b80d6435711b56d8b3286a
ivanapanowicz <ivanapanowicz@gmail.com>
2024-08-21 12:18:17 -0300

    Ej 6 LINQ

---
+++ src/Web/Controllers/Ej6LINQController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej6LINQController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get([FromQuery] List<string> words)
+        {
+            var query = words.Select(w => w.Replace("ia", "*"));
+            return query.ToList();
+        }
+    }
+}
