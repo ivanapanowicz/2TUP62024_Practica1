489a57375be9ecc90001f3d5cc43839f9a5064bd
ivanapanowicz <ivanapanowicz@gmail.com>
2024-08-21 12:16:29 -0300

    Ej 4 LINQ

---
+++ src/Web/Controllers/Ej4LINQController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4LINQController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<int> Get([FromQuery] List<int> list)
+        {
+            var query = list.OrderByDescending(n => n);
+            return query;
+        }
+    }
+}
